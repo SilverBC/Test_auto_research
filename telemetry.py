6970e38f949b335302b6acf3d676b4f7c76c3152
gvalson <arsidze@gmail.com>
2023-10-09 17:47:32 +0400

    Fix the process issue in the telemetry script.
    The problem was using run() instead of start().

--- telemetry.py
+++ telemetry.py
@@ -7,31 +7,38 @@ import threading
 def monitor(pid):
     perf_data = []
 
-    try:
-        while psutil.pid_exists(pid):
-            print("we are in bois")
-            cpu_percent = psutil.cpu_percent(interval=None)
-            sw_mem_info = psutil.swap_memory()
-            virt_mem_info = psutil.virtual_memory()
+    # Purposefully ignoring the intial 0.0 read.
+    psutil.cpu_percent(interval=None)
 
-            perf_data.append({
-                'cpu_percent': cpu_percent,
-                'memory_info': {
-                    'swap_percent': sw_mem_info.percent,
-                    'virtual_percent': virt_mem_info.percent
-                },
-                'timestamp': int(time.time())
-            })
+    while psutil.pid_exists(pid):
+        cpu_percent = psutil.cpu_percent(interval=None)
+        sw_mem_info = psutil.swap_memory()
+        virt_mem_info = psutil.virtual_memory()
 
-            time.sleep(1)  # Wait for 1 second between measurements
-    except KeyboardInterrupt:
-        pass
+        perf_data.append({
+            'cpu_percent': cpu_percent,
+            'memory': {
+                'swap_percent': sw_mem_info.percent,
+                'virtual_percent': virt_mem_info.percent
+            },
+            'timestamp': int(time.time())
+        })
+
+        time.sleep(1)  # Wait for 1 second between measurements
+
     print(json.dumps(perf_data))
 
 def run():
-    external_process = subprocess.Popen(['/usr/bin/mvn', '-B', 'test'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # TODO: why does python keep the external process alive???
+    external_process = subprocess.Popen(['/usr/bin/mvn', '-B', 'test'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     pid = external_process.pid
     print(pid)
     monitor_thread = threading.Thread(target=monitor, args=[pid])
-    monitor_thread.run()
+    monitor_thread.start()
+    stdout, stderr = external_process.communicate()
+    external_process.wait()
+    external_process.terminate()
+    monitor_thread.join()
+    print(stdout)
+
+if __name__ == "__main__":
+    run()
