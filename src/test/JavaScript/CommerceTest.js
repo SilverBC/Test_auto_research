6bb27d87be2a680c32f19df245cdfc332307a4be
gvalson <arsidze@gmail.com>
2023-10-10 21:17:18 +0400

    Add headless to JS test, fix indentation

--- src/test/JavaScript/CommerceTest.js
+++ src/test/JavaScript/CommerceTest.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const { describe, it, after } = require('mocha');
 
 describe('CommerceTest', function() {
-	this.timeout(15000);
+    this.timeout(15000);
 
     const USERNAME = "standard_user";
     const LOCKEDUSERNAME = "locked_out_user";
@@ -16,11 +16,11 @@ describe('CommerceTest', function() {
     let actions;
 
     async function setUp(url) {
-        const chromeOptions = new chrome.Options();
+        const chromeOptions = new chrome.Options().addArguments('--headless=new');
         driver = await new Builder()
-              .forBrowser('chrome')
-              .setChromeOptions(chromeOptions)
-              .build();
+            .forBrowser('chrome')
+            .setChromeOptions(chromeOptions)
+            .build();
         await driver.manage().setTimeouts({implicit: 4000});
         await driver.manage().window().setRect({width: 1920, height: 1080});
         wait = driver.wait;
@@ -46,8 +46,8 @@ describe('CommerceTest', function() {
     }
 
     // after(async function() {
-        // await logOut();
-        // await driver.quit();
+    // await logOut();
+    // await driver.quit();
     // });
 
     it('should run test', async function() {
@@ -80,116 +80,116 @@ describe('CommerceTest', function() {
         const successIsDisplayed = await driver.findElement(By.id('checkout_complete_container')).isDisplayed();
         assert(successIsDisplayed);
 
-		await logOut();
+	await logOut();
         await driver.quit();
     });
 
-	it('assert item name on item info page', async function() {
-		await setUp('https://www.saucedemo.com');
-		await logIn(USERNAME, PASSWORD);
+    it('assert item name on item info page', async function() {
+	await setUp('https://www.saucedemo.com');
+	await logIn(USERNAME, PASSWORD);
 	
-		await driver.findElement(By.id('item_4_title_link')).click();
+	await driver.findElement(By.id('item_4_title_link')).click();
 	
-		const itemName = await driver.findElement(By.className('inventory_details_name')).getText();
-		assert.strictEqual(itemName, 'Sauce Labs Backpack');
-		
-		const itemDesc = await driver.findElement(By.className('inventory_details_desc')).getText();
-		const expectedDesc = "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.";
-		assert.strictEqual(itemDesc, expectedDesc);
+	const itemName = await driver.findElement(By.className('inventory_details_name')).getText();
+	assert.strictEqual(itemName, 'Sauce Labs Backpack');
+	
+	const itemDesc = await driver.findElement(By.className('inventory_details_desc')).getText();
+	const expectedDesc = "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.";
+	assert.strictEqual(itemDesc, expectedDesc);
 
-		await logOut();
+	await logOut();
         await driver.quit();
-	});
+    });
+    
+    it('Move To LinkedIn', async function() {
+	await setUp('https://www.saucedemo.com');
+	await logIn(USERNAME, PASSWORD);
 	
-	it('Move To LinkedIn', async function() {
-		await setUp('https://www.saucedemo.com');
-		await logIn(USERNAME, PASSWORD);
-	
-		const linkedInElement = await driver.findElement(By.className('social_linkedin'));
-		await driver.actions().move({origin: linkedInElement}).click().perform();	
+	const linkedInElement = await driver.findElement(By.className('social_linkedin'));
+	await driver.actions().move({origin: linkedInElement}).click().perform();	
 
-		const handles = await driver.getAllWindowHandles();
-		console.log("Window handles: ", handles);
-		await driver.switchTo().window(handles[1]);
+	const handles = await driver.getAllWindowHandles();
+	console.log("Window handles: ", handles);
+	await driver.switchTo().window(handles[1]);
 	
-		const currentURL = await driver.getCurrentUrl();
-		assert.strictEqual(currentURL, 'https://www.linkedin.com/company/sauce-labs/');
+	const currentURL = await driver.getCurrentUrl();
+	assert.strictEqual(currentURL, 'https://www.linkedin.com/company/sauce-labs/');
 	
-		await driver.close();
-		await driver.switchTo().window(handles[0]);
+	await driver.close();
+	await driver.switchTo().window(handles[0]);
 
-		await logOut();
+	await logOut();
         await driver.quit();
-	});
+    });
 
-	it('Assert Sorting', async function() {
-		await setUp('https://www.saucedemo.com');
-		await logIn(USERNAME, PASSWORD);
+    it('Assert Sorting', async function() {
+	await setUp('https://www.saucedemo.com');
+	await logIn(USERNAME, PASSWORD);
 	
-		// Click the filters box
-		await driver.findElement(By.className("select_container")).click();
-		// Click Z-A
-		await driver.findElement(By.css("option[value='za']")).click();
-		// Check that the name at the top is "Test.allTheThings() T-Shirt (Red)"
-		let itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
-		assert.strictEqual(itemName, "Test.allTheThings() T-Shirt (Red)");
+	// Click the filters box
+	await driver.findElement(By.className("select_container")).click();
+	// Click Z-A
+	await driver.findElement(By.css("option[value='za']")).click();
+	// Check that the name at the top is "Test.allTheThings() T-Shirt (Red)"
+	let itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
+	assert.strictEqual(itemName, "Test.allTheThings() T-Shirt (Red)");
 	
-		// Click the filters box
-		await driver.findElement(By.className("select_container")).click();
-		// Click A-Z 
-		await driver.findElement(By.css("option[value='az']")).click();
-		// Check that the name at the top is "Sauce Labs Backpack"
-		itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
-		assert.strictEqual(itemName, "Sauce Labs Backpack");
+	// Click the filters box
+	await driver.findElement(By.className("select_container")).click();
+	// Click A-Z 
+	await driver.findElement(By.css("option[value='az']")).click();
+	// Check that the name at the top is "Sauce Labs Backpack"
+	itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
+	assert.strictEqual(itemName, "Sauce Labs Backpack");
 	
-		// Click the filters box
-		await driver.findElement(By.className("select_container")).click();
-		// Click low-High
-		await driver.findElement(By.css("option[value='hilo']")).click();
-		// Check that the name at the top is "Sauce Labs Onesie"
-		itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
-		assert.strictEqual(itemName, "Sauce Labs Fleece Jacket");
+	// Click the filters box
+	await driver.findElement(By.className("select_container")).click();
+	// Click low-High
+	await driver.findElement(By.css("option[value='hilo']")).click();
+	// Check that the name at the top is "Sauce Labs Onesie"
+	itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
+	assert.strictEqual(itemName, "Sauce Labs Fleece Jacket");
 	
-		// Click the filters box
-		await driver.findElement(By.className("select_container")).click();
-		// Click high-Low
-		await driver.findElement(By.css("option[value='lohi']")).click();
-		// Check that the name at the top is "Sauce Labs Fleece Jacket"
-		itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
-		assert.strictEqual(itemName, "Sauce Labs Onesie");
+	// Click the filters box
+	await driver.findElement(By.className("select_container")).click();
+	// Click high-Low
+	await driver.findElement(By.css("option[value='lohi']")).click();
+	// Check that the name at the top is "Sauce Labs Fleece Jacket"
+	itemName = await driver.findElement(By.css(".inventory_item_name")).getText();
+	assert.strictEqual(itemName, "Sauce Labs Onesie");
 	
-		await logOut();
+	await logOut();
         await driver.quit();
-	});
+    });
+    
+    it('Assert Shopping Button States', async function() {
+	await setUp('https://www.saucedemo.com');
+	await logIn(USERNAME, PASSWORD);
 	
-	it('Assert Shopping Button States', async function() {
-		await setUp('https://www.saucedemo.com');
-		await logIn(USERNAME, PASSWORD);
+	let buttonsToAdd = await driver.findElements(By.css("[id^=add-to-cart]"));
+	for(let button of buttonsToAdd) {
+	    await driver.actions().move({origin: button}).perform();
+	    await button.click();
+	}
 	
-		let buttonsToAdd = await driver.findElements(By.css("[id^=add-to-cart]"));
-		for(let button of buttonsToAdd) {
-			await driver.actions().move({origin: button}).perform();
-			await button.click();
-		}
+	// Check that the cart button says 6
+	let cartCount = await driver.findElement(By.className("shopping_cart_badge")).getText();
+	assert.strictEqual(cartCount, "6");
 	
-		// Check that the cart button says 6
-		let cartCount = await driver.findElement(By.className("shopping_cart_badge")).getText();
-		assert.strictEqual(cartCount, "6");
+	let buttonsToRemove = await driver.findElements(By.css("[id^=remove]"));
+	for(let button of buttonsToRemove) {
+	    await driver.actions().move({origin: button}).perform();
+	    await button.click();
+	}
 	
-		let buttonsToRemove = await driver.findElements(By.css("[id^=remove]"));
-		for(let button of buttonsToRemove) {
-			await driver.actions().move({origin: button}).perform();
-			await button.click();
-		}
+	// Check that the cart button says nothing
+	let cartBadges = await driver.findElements(By.className("shopping_cart_badge"));
+	assert(cartBadges.length < 1);
 	
-		// Check that the cart button says nothing
-		let cartBadges = await driver.findElements(By.className("shopping_cart_badge"));
-		assert(cartBadges.length < 1);
-	
-		await logOut();
+	await logOut();
         await driver.quit();
-	});
-	
+    });
+    
     it('Assert User Log In Cache Permanency', async function() { 
         await setUp('https://www.saucedemo.com');
         await logIn(USERNAME, PASSWORD);
@@ -208,7 +208,7 @@ describe('CommerceTest', function() {
         const cartBadgeText = await driver.findElement(By.className('shopping_cart_badge')).getText();
         assert.strictEqual(cartBadgeText, '2');
 
-		await logOut();
+	await logOut();
         await driver.quit();
     });
 
@@ -242,9 +242,9 @@ describe('CommerceTest', function() {
 
         await logOut();
         await driver.quit();
-	});
+    });
 
-	it('Assert Error Accessing Without Log In', async function() {
+    it('Assert Error Accessing Without Log In', async function() {
         await setUp('https://www.saucedemo.com/cart.html');  // Direct link
 
         const errorMessage = await driver.findElement(By.className('error-message-container')).getText();
@@ -258,7 +258,7 @@ describe('CommerceTest', function() {
         const cartContainerDisplayed = await driver.findElement(By.id('cart_contents_container')).isDisplayed();
         assert(cartContainerDisplayed);
 
-		await logOut();
+	await logOut();
         await driver.quit();
     });
 
@@ -278,11 +278,11 @@ describe('CommerceTest', function() {
 
         await logIn(USERNAME, PASSWORD);
 
-		await logOut();
+	await logOut();
         await driver.quit();
     });
 
-	it('Glitched User Experience', async function() {
+    it('Glitched User Experience', async function() {
         await setUp('https://www.saucedemo.com');
         await logIn(GLITCHUSERNAME, PASSWORD);
 
@@ -312,7 +312,7 @@ describe('CommerceTest', function() {
         const successIsDisplayed = await driver.findElement(By.id('checkout_complete_container')).isDisplayed();
         assert(successIsDisplayed);
 
-		await logOut();
+	await logOut();
         await driver.quit();
     });
 
