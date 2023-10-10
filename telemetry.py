17b2cc81726fc7bffda1776ccc1c0ad4d6143425
gvalson <arsidze@gmail.com>
2023-10-10 20:20:30 +0400

    Add language parametrization to the script

--- telemetry.py
+++ telemetry.py
@@ -3,24 +3,33 @@ import time
 import subprocess
 import json
 import threading
+import os
+import shlex
+
+launch_scripts = {
+    "java": shlex.split("mvn -B test"),
+    "java_comp": shlex.split("mvn -B clean test"),
+    "c#": shlex.split("dotnet test src/test/C\\#/packages.csproj"),
+    "c#_comp": shlex.split("dotnet test src/test/C\\#/packages.csproj"),
+    "js": shlex.split("npm --prefix src/test/JavaScript test"),
+    "python": shlex.split("pytest src/test/python/CommerceTest.py"),
+    "ruby": shlex.split("ruby src/test/Ruby/CommerceTest.rb")
+}
 
 def monitor(pid):
     perf_data = []
 
-    # Purposefully ignoring the intial 0.0 read.
-    psutil.cpu_percent(interval=None)
-
     while psutil.pid_exists(pid):
+        # Purposefully ignoring the intial 0.0 read.
+        psutil.cpu_percent(interval=None)
         cpu_percent = psutil.cpu_percent(interval=None)
         sw_mem_info = psutil.swap_memory()
         virt_mem_info = psutil.virtual_memory()
 
         perf_data.append({
             'cpu_percent': cpu_percent,
-            'memory': {
-                'swap_percent': sw_mem_info.percent,
-                'virtual_percent': virt_mem_info.percent
-            },
+            'memory_swap': sw_mem_info.percent,
+            'memory_virtual': virt_mem_info.percent
             'timestamp': int(time.time())
         })
 
@@ -29,7 +38,9 @@ def monitor(pid):
     print(json.dumps(perf_data))
 
 def run():
-    external_process = subprocess.Popen(['/usr/bin/mvn', '-B', 'test'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    target_language = os.environ["TARGET_LANGUAGE"]
+    launch_script = launch_scripts[target_language]
+    external_process = subprocess.Popen(launch_script, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     pid = external_process.pid
     print(pid)
     monitor_thread = threading.Thread(target=monitor, args=[pid])
